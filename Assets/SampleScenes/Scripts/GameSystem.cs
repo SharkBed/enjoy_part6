f9c42d28a8ebc8b959f2aabd2f27a463621a791c
ASOST\1601321 <idek1601321@gmail.com>
2018-07-30 15:05:51 +0900

    シーン遷移完璧かな

--- Assets/SampleScenes/Scripts/GameSystem.cs
+++ Assets/SampleScenes/Scripts/GameSystem.cs
@@ -1,23 +1,35 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
 
 public class GameSystem : MonoBehaviour
 {
-    public void Start()
-    {
-        AudioManager.Instance.PlayBGM("Title");
-    }
-
-    public void Update()
-    {
-    }
 
     //　スタートボタンを押したら実行する
     public void GameStart()
     {
-        AudioManager.Instance.StopBGM();
         SceneManager.LoadScene("NetworkLobby");
+    }
+    //　ゲーム終了ボタンを押したら実行する
+    public void GameEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+		Application.Quit();
+#endif
+    }
+    public void Title()
+    {
+        SceneManager.LoadScene("title");
+        //LobbyManager.s_Singleton.ServerReturnToLobby();
+    }
+    public void Lobby()
+    {
+        LobbyManager.s_Singleton.ServerReturnToLobby();
     }
 }
